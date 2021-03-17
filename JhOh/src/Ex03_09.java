bcb8ecbca691123d2c18e7cd7ddd8aaa97d35b3e
data08 <data08@DESKTOP-SUVEFO9>
2021-03-17 11:28:36 +0900

    반복 연습

---
+++ JhOh/src/Ex03_09.java
@@ -0,0 +1,35 @@
+
+public class Ex03_09 {
+
+	public static void main(String[] args) {
+
+		int a, b, c, d;
+		
+		a = 100 + 100;
+		b = a + 100;
+		c = a + b - 100;
+		d = a + b + c;
+		
+		System.out.println("a : " + a);
+		System.out.println("b : " + b);
+		System.out.println("c : " + c);
+		System.out.println("d : " + d);
+		
+		a = b = c = d = 100;
+		
+		System.out.println("a : " + a);
+		System.out.println("b : " + b);
+		System.out.println("c : " + c);
+		System.out.println("d : " + d);
+		
+		a = 100;
+		a = a + 200;
+		
+		System.out.println("a : " + a);
+		
+		a = 10;
+		System.out.println(a);
+	
+	}
+
+}
